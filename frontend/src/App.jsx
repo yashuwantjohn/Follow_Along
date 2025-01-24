40543c435d4c453be94e178812ed07b8be177fc7
indhuja-elumalai <indhuja1012006@gmail.com>
2025-01-24 09:11:13 +0530

    committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {LoginPage,SignupPage} from './Routes';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,SignupPage,Home} from "./Routes";
+import "./App.css";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
       <Route path="/signup" element={<SignupPage />} />
     </Routes>
     </BrowserRouter>
-  );
+    
+  )
 }
 
-export default App;
+export default App
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,3 +1,4 @@
-import LoginPage from "./pages/login";
-import SignupPage from "./pages/Signup";
-export {LoginPage,SignupPage};
+import LoginPage from './pages/Login'
+import SignupPage from './pages/Signup'
+import Home from './pages/Home';
+export {LoginPage,SignupPage,Home};
