d071c01ba011346fc4047dd34d1db386314a2e01
indhuja-elumalai <indhuja1012006@gmail.com>
2025-01-29 14:28:23 +0530

    milestone-9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,SignupPage,Home} from "./Routes";
+import {LoginPage,SignupPage,Home,CreateProduct} from "./Routes";
 import "./App.css";
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<LoginPage/>}/>
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/create-product" element={<CreateProduct />} />
     </Routes>
     </BrowserRouter>
     
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,4 +1,5 @@
 import LoginPage from './pages/Login'
 import SignupPage from './pages/Signup'
-import Home from './pages/Home';
-export {LoginPage,SignupPage,Home};
+import Home from "./pages/Home";
+import CreateProduct from './pages/createProduct.jsx';
+export {LoginPage,SignupPage,Home,CreateProduct};
